c4c9de0c1a2947c4963b803b2e52929256bdf1cc
RomanG <roma@gmail.com>
2023-10-30 14:13:59 +0300

    fisrt commit

---
+++ src/test/java/guru/qa/Start.java
@@ -0,0 +1,25 @@
+package guru.qa;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Start {
+    @Test
+    void name() {
+        Assertions.assertTrue(3>2);
+    }
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+
+}
