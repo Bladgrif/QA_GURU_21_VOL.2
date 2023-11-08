d9fce89cce01179fcc74113506da8b8ec8393f7d
RomanG <roma@gmail.com>
2023-11-08 17:01:32 +0300

    commit 08.11

--- src/test/java/com/demoqa/forms/practice_form.java
+++ src/test/java/com/demoqa/forms/practice_form.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -28,6 +30,12 @@ public class practice_form {
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("May");
 //        $(".react-datepicker__month-select").selectOptionByValue("5");
+        $(".react-datepicker__year-select").selectOption("2007");
+        $("[aria-label='Choose Sunday, May 6th, 2007']").click();
+        $("#subjectsInput").setValue("Accounting").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("src/test/resources/1.png");
+        $("#currentAddress").setValue("Russia street");
 
 
         sleep(10000);
