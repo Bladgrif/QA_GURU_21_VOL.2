64697d2ed9ec4785941d7e2e6ded77a697e66588
RomanG <roma@gmail.com>
2023-11-13 15:24:08 +0300

    Закончил тест на регистрацию формы

--- src/test/java/com/demoqa/forms/practice_form.java
+++ src/test/java/com/demoqa/forms/practice_form.java
@@ -5,6 +5,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -19,35 +21,61 @@ public class practice_form {
     @Test
     void sucessfullRegistrationFormTest() {
         open("/automation-practice-form");
+
+        //Удаление банера
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
+
+        //Заполнение простых полей
         $("#firstName").setValue("Roman");
         $("#lastName").setValue("Grigorev");
         $("#userEmail").setValue("Grigorev@mail.ru");
         $("#userNumber").setValue("0123456789");
+        $("#currentAddress").setValue("Russia street");
 
+        //Заполение пола
         $("#genterWrapper").find(byText("Female")).click(); //good
 //        $("#gender-radio-3").parent().click(); //good
 
+        //Работа с календарем
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("May");
 //        $(".react-datepicker__month-select").selectOptionByValue("5");
         $(".react-datepicker__year-select").selectOption("2007");
         $("[aria-label='Choose Sunday, May 6th, 2007']").click();
+
+        //Выбор хобби
         $("#subjectsInput").setValue("Accounting").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
+
+        //Загрузка картинки
         $("#uploadPicture").uploadFromClasspath("imj/Scr.png");
-        $("#currentAddress").setValue("Russia street");
+
+        //Выбор города
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
 
-        $(".modal-content").shouldBe(Condition.visible);
+        //Первичаня проверка заполненной формы
+        $(".modal-content").should(appear);
+//        $(".modal-content").shouldBe(Condition.visible);
+        $("#example-modal-sizes-title-lg").shouldBe(text("Thanks for submitting the form"));
+
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Roman Grigorev"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("Grigorev@mail.ru"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Female"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("0123456789"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("Date of Birth 06 May,2007"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Accounting"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("Scr.png"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Russia street"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
 
 
 
-        sleep(10000);
+//        sleep(10000);
     }
 }
