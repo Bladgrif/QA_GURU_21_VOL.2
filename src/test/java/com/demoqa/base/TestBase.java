07379a34f75a03403a61be299ee70fe88d39c233
RomanG <roma@gmail.com>
2023-11-15 10:29:00 +0300

    extends base test

---
+++ src/test/java/com/demoqa/base/TestBase.java
@@ -0,0 +1,17 @@
+package com.demoqa.base;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Configuration.baseUrl;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+}
