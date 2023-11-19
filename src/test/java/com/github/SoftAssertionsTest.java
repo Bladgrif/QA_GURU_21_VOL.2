bc812daac09a947c6508615c19a70e4e401cf33d
Badgrif <aswd@asd.com>
2023-11-19 20:25:07 +0300

    added SoftAssertionsTest

---
+++ src/test/java/com/github/SoftAssertionsTest.java
@@ -0,0 +1,42 @@
+package com.github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Configuration.holdBrowserOpen;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertionsTest {
+
+    String junit5 = "@ExtendWith({SoftAssertsExtension.class})\n" +
+            "class Tests {\n" +
+            "  @Test\n" +
+            "  void test() {\n" +
+            "    Configuration.assertionMode = SOFT;\n" +
+            "    open(\"page.html\");\n" +
+            "\n" +
+            "    $(\"#first\").should(visible).click();\n" +
+            "    $(\"#second\").should(visible).click();\n" +
+            "  }\n" +
+            "}";
+
+    @BeforeAll
+    static void beforeAll() {
+        baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void findAssertions() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-box").$(".wiki-more-pages-link button").click();
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("#wiki-body").$(byText("3. Using JUnit5 extend test class:")).sibling(0).shouldHave(text(junit5));
+    }
+}
